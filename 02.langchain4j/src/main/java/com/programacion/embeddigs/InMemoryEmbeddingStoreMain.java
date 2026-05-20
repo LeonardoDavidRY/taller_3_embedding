52f3c5eed3911b4eae3758a1a214a0c2a4ee3faf
LeonardoDavidRY <ldramirez@uce.edu.ec>
2026-05-20 10:24:33 -0500

    Clase 20-05-26

---
+++ 02.langchain4j/src/main/java/com/programacion/embeddigs/InMemoryEmbeddingStoreMain.java
@@ -0,0 +1,46 @@
+package com.programacion.embeddigs;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
+import dev.langchain4j.store.embedding.EmbeddingSearchResult;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.util.List;
+
+public class InMemoryEmbeddingStoreMain {
+
+    static void main() {
+        var documentos = List.of(
+                "Java is an object-oriented programming language",
+                "Python is popular for artificial intelligence",
+                "The coffee machine is in the kitchen",
+                "Embeddings represent text as vectors"
+        );
+        var model = new AllMiniLmL6V2EmbeddingModel();
+
+        InMemoryEmbeddingStore<TextSegment> store = new InMemoryEmbeddingStore<>();
+
+        documentos.forEach(it-> {
+                    String key = "doc-"+(store.size() +1);
+                    var embedding = model.embed(it);
+
+                    store.add(key, embedding.content(), TextSegment.from(it));
+                });
+        String consulta = "object oriented programmig";
+
+        var searchQuery = EmbeddingSearchRequest.builder()
+                .queryEmbedding(model.embed(consulta).content())
+                .maxResults(2) //top-k
+                .build();
+
+        EmbeddingSearchResult<TextSegment> searchResult = store.search(searchQuery);
+
+        for(var it : searchResult.matches()) {
+            //System.out.println(it);
+
+            System.out.printf("Score: %.2f - %s%n", it.score(), it.embedded().text() );
+        }
+
+    }
+}
